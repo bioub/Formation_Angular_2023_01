04f0dae804c97f6aea7be096d6c3b50ca44bd4d7
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-01-19 15:29:50 +0100

    Router 2e partie

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { TodosModule } from './todos/todos.module';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
     BrowserModule, // importe également CommonModule
 
     TodosModule,
+    UsersModule,
     AppRoutingModule, // importe également RouterModule en dernier à cause du path: '**'
   ],
   providers: [],
---
+++ my-app/src/app/users/user-add/user-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-user-add',
+  templateUrl: './user-add.component.html',
+  styleUrls: ['./user-add.component.scss']
+})
+export class UserAddComponent {
+
+}
